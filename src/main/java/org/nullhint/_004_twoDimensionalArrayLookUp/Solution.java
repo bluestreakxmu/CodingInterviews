701abacef0d26384d9317277ceef389b7c486ce0
lixibo <alanlxb.xmu@gmail.com>
2019-03-22 12:35:17 +0800

    题目四：二维数组中的查找

--- src/main/java/org/nullhint/_004_twoDimensionalArrayLookUp/Solution.java
+++ src/main/java/org/nullhint/_004_twoDimensionalArrayLookUp/Solution.java
@@ -10,19 +10,30 @@ public class Solution {
     /**
      * 判断二维数组中是否含有该整数。
      * <p>
-     * 解题思路：
+     * 解题思路：从数据右上角（或左下角）开始比较。
      * <p>
-     * 时间复杂度：O()，空间复杂度：O()。
+     * 时间复杂度：O(2n)，空间复杂度：O(1)。
      *
      * @param array 二维数组
      * @param num   要查找的数字
      * @return true-有，false-没有
      */
     public static boolean solve(int[][] array, int num) {
-        if (array == null) {
+        if (array == null || array.length <= 0) {
             return false;
         }
 
+        int x = 0;
+        int y = array[0].length - 1;
+        while (y >= 0 && x <= array.length - 1) {
+            if (num == array[x][y]) {
+                return true;
+            } else if (num < array[x][y]) {
+                y--;
+            } else {
+                x++;
+            }
+        }
 
         return false;
     }
