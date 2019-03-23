e1d2dd4d0e9f6206f69240788c5addc6501a6e81
lixibo <alanlxb.xmu@gmail.com>
2019-03-23 10:11:12 +0800

    题目十：斐波那契数列变形题-青蛙跳台问题。

--- src/main/java/org/nullhint/_010_fibonacci/Solution.java
+++ src/main/java/org/nullhint/_010_fibonacci/Solution.java
@@ -25,4 +25,29 @@ public class Solution {
 
         return next;
     }
+
+    /**
+     * 青蛙跳台问题解法。<br>
+     * 解题思路：原理也是斐波那契数列。<br>
+     * 时间复杂度：O(n)，空间复杂度：O(1)。
+     *
+     * @param n 第n级台阶
+     * @return 跳法总数
+     */
+    public static int solve2(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int one = 1;
+        int two = 2;
+        int next = 0;
+        for (int i = 3; i <= n; i++) {
+            next = one + two;
+            one = two;
+            two = next;
+        }
+
+        return next;
+    }
 }
--- src/test/java/org/nullhint/_010_fibonacci/SolutionTest.java
+++ src/test/java/org/nullhint/_010_fibonacci/SolutionTest.java
@@ -2,10 +2,12 @@ package org.nullhint._010_fibonacci;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
-
+    /**
+     * 斐波那契数列单元测试。
+     */
     @Test
     public void solve1() {
         assertEquals(Solution.solve1(0), 0);
@@ -13,5 +15,17 @@ public class SolutionTest {
         assertEquals(Solution.solve1(2), 1);
         assertEquals(Solution.solve1(3), 2);
         assertEquals(Solution.solve1(12), 144);
+    }
+
+    /**
+     * 青蛙跳台问题单元测试。
+     */
+    @Test
+    public void solve2() {
+        assertEquals(Solution.solve2(0), 0);
+        assertEquals(Solution.solve2(1), 1);
+        assertEquals(Solution.solve2(2), 2);
+        assertEquals(Solution.solve2(3), 3);
+        assertEquals(Solution.solve2(11), 144);
     }
 }
