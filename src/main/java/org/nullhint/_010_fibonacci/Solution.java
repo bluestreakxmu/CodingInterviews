2020480d2cfc450c71afbe8764bc3f780d4a6e54
lixibo <alanlxb.xmu@gmail.com>
2019-03-23 09:22:37 +0800

    题目十：求斐波那契数列的第n项解法。

--- src/main/java/org/nullhint/_010_fibonacci/Solution.java
+++ src/main/java/org/nullhint/_010_fibonacci/Solution.java
@@ -3,12 +3,26 @@ package org.nullhint._010_fibonacci;
 public class Solution {
 
     /**
-     * 求斐波那契数列第n项的值。
+     * 求斐波那契数列第n项的值。<br>
+     * 时间复杂度：O(n)，空间复杂度：O(1)。
      *
      * @param n 第n项
      * @return 第n项的值
      */
-    public static int solve1(int n)  {
-        return -1;
+    public static long solve1(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        long one = 0;
+        long two = 1;
+        long next = 1;
+        for (int i = 2; i <= n; i++) {
+            next = one + two;
+            one = two;
+            two = next;
+        }
+
+        return next;
     }
 }
